5200270f7fa3caa80d0d8d163596eaedff52dd70
{nicu-chiriac} <{nicu.chiriac99@yahoo.com}>
2022-03-08 15:34:40 +0200

    Stage 1

---
+++ src/App.js
@@ -0,0 +1,99 @@
+import React, { Component } from 'react';
+import Particles from "react-tsparticles";
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm.js';
+import Rank from './components/Rank/Rank.js';
+import './App.css';
+
+const particlesOptions = {
+  fpsLimit: 60,
+  interactivity: {
+    events: {
+      onClick: {
+        enable: true,
+        mode: "push",
+      },
+      onHover: {
+        enable: true,
+        mode: "repulse",
+      },
+      resize: true,
+    },
+    modes: {
+      bubble: {
+        distance: 400,
+        duration: 4,
+        opacity: 0.2,
+        size: 40,
+      },
+      push: {
+        quantity: 4,
+      },
+      repulse: {
+        distance: 50,
+        duration: 2,
+      },
+    },
+  },
+  particles: {
+    color: {
+      value: "#ffffff",
+    },
+    links: {
+      color: "#ffffff",
+      distance: 150,
+      enable: true,
+      opacity: 0.5,
+      width: 1,
+    },
+    collisions: {
+      enable: true,
+    },
+    move: {
+      direction: "none",
+      enable: true,
+      outMode: "bounce",
+      random: false,
+      speed: 1,
+      straight: false,
+    },
+    number: {
+      density: {
+        enable: true,
+        area: 800,
+      },
+      value: 80,
+    },
+    opacity: {
+      value: 0.5,
+    },
+    shape: {
+      type: "circle",
+    },
+    size: {
+      random: true,
+      value: 2,
+    },
+  },
+  detectRetina: true,
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Particles className='particles'  
+        params={particlesOptions}
+        />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm />
+        {/*<FaceRecognition />*/}
+      </div>
+    );
+  }
+}
+
+export default App;
